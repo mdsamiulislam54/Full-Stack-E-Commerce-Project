dd99c7373cef0a4897f30a5fef1bd48c2959d87e
mdsamiulislam54 <samiulm5332@gmail.com>
2025-03-19 15:10:04 +0600

    Testimonial section update

--- client/dailymart/src/componants/Testimonial/Testimonial.jsx
+++ client/dailymart/src/componants/Testimonial/Testimonial.jsx
@@ -1,7 +1,105 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/grid";
+import "swiper/css/pagination";
+import { Grid, Pagination } from "swiper/modules";
+import "./style.css";
 const Testimonial = () => {
+  const [testimonial, setTestimonial] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/testimonial.json");
+        const data = await response.json();
+        setTestimonial(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="py-10">
-      <div className="w-11/12 mx-auto">Testimonial</div>
+    <div className="py-20 bg-assent">
+      <div className="w-11/12 mx-auto">
+        <div className="flex flex-col gap-4 items-center justify-center  pb-10 ">
+          <h2 className="font-dm-snas text-4xl font-semibold tracking-wide text-center">
+            Our satisfied customer says
+          </h2>
+          <p className="text-center text-sm tracking-wide">
+            Far far away, behind the word mountains, far from the countries
+            <br></br>
+            Vokalia and Consonantia, there live the blind texts. Separated they
+            live in
+          </p>
+        </div>
+        <div className="py-5">
+          <Swiper
+            slidesPerView={3}
+            grid={{
+              fill: "row",
+              rows: 2,
+            }}
+            
+            spaceBetween={30}
+            pagination={{
+              clickable: true,
+            }}
+            modules={[Grid, Pagination]}
+            className="swiper"
+            breakpoints={{
+                320: {
+                  slidesPerView: 1, // Mobile devices
+                },
+                640: {
+                  slidesPerView: 2, // Small screens
+                },
+                768: {
+                  slidesPerView: 3, // Medium screens
+                },
+                1024: {
+                  slidesPerView: 4, // Large screens
+                },
+              }}
+          >
+            {testimonial.map((item, index) => (
+              <SwiperSlide key={index} className="swiper-slide mb-10 ">
+                <div className="flex flex-col gap-4 items-center justify-center shadow  rounded-md p-2">
+                  <div className="relative w-20 h-20 rounded-full overflow-hidden shadow-sm">
+                    <img
+                      className="w-full h-full object-cover hover:scale-125 transition-all duration-300"
+                      src={item.images}
+                      alt={item.name}
+                    />
+                  </div>
+                  <p className="text-[12px] tracking-wide text-center text-gray-500">
+                    {item.date}
+                  </p>
+                  <p className="text-xl font-semibold  tracking-wider text-center">
+                    {item.name}
+                  </p>
+                  <p className="text-sm tracking-wider text-center">
+                    {item.message}
+                  </p>
+                  <p className="text-md tracking-wider text-center">
+                    {item.email}
+                  </p>
+                  <p className="text-md tracking-wider text-center">
+                    {item.phone}
+                  </p>
+                  <p className="text-md tracking-wider text-center">
+                    {item.city}
+                  </p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      </div>
     </div>
   );
 };
