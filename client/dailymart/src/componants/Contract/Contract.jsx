4693366382c2a7dcb3289483366b729af24fc68a
mdsamiulislam54 <samiulm5332@gmail.com>
2025-04-12 15:32:32 +0600

    Contact section from submit function update

--- client/dailymart/src/componants/Contract/Contract.jsx
+++ client/dailymart/src/componants/Contract/Contract.jsx
@@ -1,8 +1,33 @@
 import { CiMobile3 } from "react-icons/ci";
 import { AiOutlineMail } from "react-icons/ai";
 import { IoLocationOutline } from "react-icons/io5";
+import Swal from 'sweetalert2'
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Contract = () => {
+    const clearfrom = useRef(null)
+    const navigate = useNavigate()
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const from = clearfrom.current
+    const name = e.target.name.value
+    Swal.fire({
+        title: `Hello, ${name}! 👋`,
+        text: 'Your form has been submitted successfully.',
+        icon: 'success',
+        confirmButtonText: 'Okay',
+        background: '#f0f9ff',
+        color: '#333',
+        confirmButtonColor: '#4C1D95',
+      
+      });
+
+      from.reset()
+      navigate('/')
+   
+  };
   return (
     <div>
       <div className="w-8/12 mx-auto my-22">
@@ -39,26 +64,31 @@ const Contract = () => {
           <h2 className="mb-4 font-dm-snas text-3xl font-medium">
             Get in Touch
           </h2>
-          <form action="">
+          <form ref={clearfrom} onSubmit={handleSubmit} action="">
             <div className="flex justify-between gap-5 mb-8">
               <input
                 type="text"
+                required
+                name="name"
                 placeholder="Your name"
                 className="w-full p-3 outline-none border border-gray-300 text-md font-light hover:border-primary transition-all duration-300 rounded"
               />
               <input
                 type="email"
                 placeholder="Your email"
+                required
                 className="w-full p-3 outline-none border border-gray-300 text-md font-light hover:border-primary transition-all duration-300 rounded"
               />
               <input
                 type="text"
+                required
                 placeholder="Your phone number"
                 className="w-full p-3 outline-none border border-gray-300 text-md font-light hover:border-primary transition-all duration-300 rounded"
               />
             </div>
             <div className="mb-4">
               <textarea
+                required
                 className="w-full outline-none border border-gray-300 text-md font-light hover:border-primary transition-all duration-300 rounded p-3 "
                 name="message"
                 id=""
@@ -67,7 +97,7 @@ const Contract = () => {
             </div>
             <input
               className="px-10 py-2 border border-gray-300 font-dm-snas text-md font-semibold bg-primary text-light hover:bg-secondary cursor-pointer transition-all duration-300 rounded-md hover:text-light"
-              type="button"
+              type="submit"
               value="Send Message"
             />
           </form>
